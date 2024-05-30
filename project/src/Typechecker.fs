d9a88e9e27b52d016ccc4e4fe351934113950e1d
s220281 <s220281@student.dtu.dk>
2024-05-30 10:32:53 +0200

    removed unnecessary code and finalised typechecker

--- project/src/Typechecker.fs
+++ project/src/Typechecker.fs
@@ -12,60 +12,32 @@ type TypeInfo =
     | TVariant of string list
     | UnknownType
 
-type Scope = {
-    Variables: Map<string, TypeInfo>
-    ChildScopes: Map<string, Scope>
-}
 
 type TypeEnvironment = {
     Variables: Map<string, TypeInfo>
     Types: Map<string, TypeInfo>
-    Scopes: Map<string, Scope>
-}
-
-let emptyScope = {
-    Variables = Map.empty
-    ChildScopes = Map.empty
 }
 
 let emptyEnvironment = {
     Variables = Map.empty
     Types = Map.empty
-    Scopes = Map.empty
 }
 
 let memo = Dictionary<string, TypeInfo>()
 
-
-let addVariableToScope (scope: Scope) (name: string) (typ: TypeInfo) = { scope with Variables = Map.add name typ scope.Variables }
-let addChildScopeToScope (scope: Scope) (name: string) (childScope: Scope) = { scope with ChildScopes = Map.add name childScope scope.ChildScopes }
-
 let addVariable (env: TypeEnvironment) (name: string) (typ: TypeInfo) = { env with Variables = Map.add name typ env.Variables }
 let addType (env: TypeEnvironment) (name: string) (typ: TypeInfo) = { env with Types = Map.add name typ env.Types }
-let addScope (env: TypeEnvironment) (name: string) (scope: Scope) = { env with Scopes = Map.add name scope env.Scopes }
 
 let mergeMaps (map1: Map<string, TypeInfo>) (map2: Map<string, TypeInfo>) =
     map2
     |> Map.fold (fun acc key value -> Map.add key value acc) map1
 
-let rec lookupVariableInScope (scope: Scope) (name: string) =
-    match Map.tryFind name scope.Variables with
-    | Some typ -> Some typ
-    | None ->
-        match Map.tryFind name scope.ChildScopes with
-        | Some childScope -> lookupVariableInScope childScope name
-        | None -> None
-
 let rec lookupVariable (env: TypeEnvironment) (name: string) =
     match Map.tryFind name env.Variables with
     | Some typ -> Some typ
-    | None ->
-        match Map.tryFind name env.Scopes with
-        | Some scope -> lookupVariableInScope scope name
-        | None -> None
+    | None -> None
 
 let lookupType (env: TypeEnvironment) (name: string) = Map.tryFind name env.Types
-let lookupScope (env: TypeEnvironment) (name: string) = Map.tryFind name env.Scopes
 
 let rec isSubtype env (subType: TypeInfo) (superType: TypeInfo) =
     match (subType, superType) with
@@ -75,6 +47,13 @@ let rec isSubtype env (subType: TypeInfo) (superType: TypeInfo) =
         match lookupType env subName with
         | Some baseType -> isSubtype env baseType superType
         | None -> false
+    | (TFunction (subParams, subReturn), TFunction (superParams, superReturn)) ->
+        List.length subParams = List.length superParams &&
+        List.forall2 (isSubtype env) subParams superParams &&
+        isSubtype env subReturn superReturn
+    | (TVariant subChoices, TVariant superChoices) ->
+        List.length subChoices = List.length superChoices &&
+        List.forall2 (fun subChoice superChoice -> subChoice = superChoice) subChoices superChoices
     | _ -> false
 
 let collectDeclarations (env: TypeEnvironment) (decl: Decl): TypeEnvironment =
@@ -93,7 +72,9 @@ let collectDeclarations (env: TypeEnvironment) (decl: Decl): TypeEnvironment =
                           | _ -> UnknownType
 
                 addType env abbrevDef.NodeCategory.name typ
-            | TypeDef.VariantDef variantDef -> addType env variantDef.NodeCategory.name (TVariant variantDef.NodeCategory.choice)
+            | TypeDef.VariantDef variantDef -> 
+                let newEnv = addType env variantDef.NodeCategory.name (TVariant variantDef.NodeCategory.choice)
+                variantDef.NodeCategory.choice |> List.fold (fun env choice -> addVariable env choice (TUserDefined variantDef.NodeCategory.name)) newEnv
         ) env
     | Decl.ValueDecl valueDecl ->
         valueDecl.NodeCategory.valueDefList |> List.fold (fun env valueDef ->
@@ -101,14 +82,16 @@ let collectDeclarations (env: TypeEnvironment) (decl: Decl): TypeEnvironment =
             | ValueDef.ValueSignature valueSig -> addVariable env valueSig.NodeCategory.name UnknownType
             | ValueDef.ExplicitValueDef valueDef -> addVariable env valueDef.NodeCategory.name UnknownType
             | ValueDef.ExplicitFunctionDef funcDef ->
-                let funcScope = List.fold (fun scope paramName -> addVariableToScope scope paramName UnknownType) emptyScope funcDef.NodeCategory.bodyExpr.NodeCategory.args
-                let envWithScope = addScope env funcDef.NodeCategory.name funcScope
-                addVariable envWithScope funcDef.NodeCategory.name (TFunction ([], UnknownType))
+                let paramTypes = funcDef.NodeCategory.args |> List.map (fun arg -> UnknownType)
+                addVariable env funcDef.NodeCategory.name (TFunction (paramTypes, UnknownType))
         ) env
     | Decl.AxiomDecl _ -> env
 
 let collectSchemeDeclarations (env: TypeEnvironment) (scheme: Node<SchemeDecl>): TypeEnvironment =
     scheme.NodeCategory.schemeDef.NodeCategory.classExpr.NodeCategory.optDecl |> List.fold collectDeclarations env
+
+let checkRelationalExprTypes (env: TypeEnvironment) (leftType: TypeInfo) (rightType: TypeInfo) =
+    isSubtype env leftType rightType || isSubtype env rightType leftType
 
 let rec resolveTypeExpr (env: TypeEnvironment) (expr: Node<TypeExpr>) (visited: Set<string>) =
     let rec loop typ visited =
@@ -137,33 +120,17 @@ let rec resolveTypeExpr (env: TypeEnvironment) (expr: Node<TypeExpr>) (visited: 
         match lookupType env t.NodeCategory.name with
         | Some typ -> loop typ (Set.add t.NodeCategory.name visited)
         | _ -> failwithf "Type not found or unknown: %s" t.NodeCategory.name
-    | TypeExpr.SubtypeExpr _ -> failwith "Subtype expressions are not supported"
+    | TypeExpr.SubtypeExpr subTypeExpr -> 
+        let singleTypingExpr = subTypeExpr.NodeCategory.singleTyping
+        let singleTyping = resolveTypeExpr env singleTypingExpr.NodeCategory.typeExpr visited
+        let newEnv = addVariable env singleTypingExpr.NodeCategory.name singleTyping
+        let valueType = resolveAndCheckValueExpr newEnv subTypeExpr.NodeCategory.valueExpr
+
+        if isSubtype newEnv valueType TBool then singleTyping else failwith "Subtype expression must result in Bool"
     | TypeExpr.BracketedTypeExpr t -> resolveTypeExpr env t.NodeCategory.typeExpr visited
 
-// let rec resolveTypeExpr (env: TypeEnvironment) (expr: Node<TypeExpr>) (currentResolving: Set<string>) : TypeInfo =
-//     match expr.NodeCategory with
-//     | TypeExpr.TypeLiteral t -> 
-//         match t.NodeCategory with
-//         | TypeLiteral.Bool -> TBool
-//         | TypeLiteral.Int -> TInt
-//     | TypeExpr.TypeName t -> 
-//         let typeName = t.NodeCategory.name
-//         if Set.contains typeName then
-//             failwithf "Circular type dependency detected: %s" typeName
-//         let newResolving = Set.add typeName
-//         let resolvedType =
-//             match lookupType env typeName with
-//             | Some typ -> typ
-//             | None -> failwithf "Type not found or unknown: %s" typeName
-//         newResolving |> Set.remove typeName |> ignore
-//         resolvedType
-//     | TypeExpr.SubtypeExpr _ -> failwith "Subtype expressions are not supported"
-//     | TypeExpr.BracketedTypeExpr t -> resolveTypeExpr env t.NodeCategory.typeExpr
 
-let checkRelationalExprTypes (env: TypeEnvironment) (leftType: TypeInfo) (rightType: TypeInfo) =
-    isSubtype env leftType rightType || isSubtype env rightType leftType
-
-let rec resolveAndCheckValueExpr (env: TypeEnvironment) (expr: Node<ValueExpr>) =
+and resolveAndCheckValueExpr (env: TypeEnvironment) (expr: Node<ValueExpr>) =
     match expr.NodeCategory with
     | ValueLiteral litNode ->
         match litNode.NodeCategory with
@@ -237,46 +204,26 @@ let rec resolveAndCheckValueExpr (env: TypeEnvironment) (expr: Node<ValueExpr>) 
             if isSubtype env exprType TInt then TInt else failwith "Type mismatch in prefix expression"
 
 let resolveAndCheckFunction env (funcDef: Node<ExplicitFunctionDef>) =
-    // Lookup the function scope created in the first pass
-    let funcScope =
-        match lookupScope env funcDef.NodeCategory.name with
-        | Some scope -> scope
-        | None -> failwithf "Scope not found for function %s" funcDef.NodeCategory.name
-    
-    printfn $"Function Scope %s{funcDef.NodeCategory.name}: %A{funcScope}"
-
     // Resolve parameter types and add to the function scope
     let paramTypes = funcDef.NodeCategory.args |> List.map (fun arg -> resolveTypeExpr env arg Set.empty)
-    printfn $"Function %s{funcDef.NodeCategory.name} param types: %A{paramTypes}"
-    let funcScope = List.fold2 (fun scope paramName paramType -> addVariableToScope scope paramName paramType) funcScope funcDef.NodeCategory.bodyExpr.NodeCategory.args paramTypes
-    printfn $"Function Scope After %s{funcDef.NodeCategory.name} with params: %A{funcScope}"
-
+   
     // Resolve return type
     let returnType = resolveTypeExpr env funcDef.NodeCategory.returnTypeExpr Set.empty
     
     // Update the function type in the original environment
     let updatedFuncType = TFunction (paramTypes, returnType)
     let updatedEnv = addVariable env funcDef.NodeCategory.name updatedFuncType
-    let newEnv = addScope updatedEnv funcDef.NodeCategory.name funcScope
 
-    printfn $"Function Env %s{funcDef.NodeCategory.name}: %A{updatedEnv}"
-
-    printfn $"New Function Env %s{funcDef.NodeCategory.name}: %A{newEnv}"
-
-    let scopedEnv = {
-        Variables = mergeMaps funcScope.Variables newEnv.Variables
-        Types = newEnv.Types
-        Scopes = funcScope.ChildScopes
-    }
+    let newEnv = List.fold2 (fun env paramName paramType -> addVariable env paramName paramType) updatedEnv funcDef.NodeCategory.bodyExpr.NodeCategory.args paramTypes
 
     // Resolve and typecheck the body expression in the function scope
-    let bodyType = resolveAndCheckValueExpr scopedEnv funcDef.NodeCategory.bodyExpr.NodeCategory.valueExpr
+    let bodyType = resolveAndCheckValueExpr newEnv funcDef.NodeCategory.bodyExpr.NodeCategory.valueExpr
     
     // Ensure the return type of the body matches the declared return type
     if not (isSubtype env bodyType returnType) then failwithf "Return type mismatch in function %s" funcDef.NodeCategory.name
     
     // Return the updated environment
-    newEnv
+    updatedEnv
     
 
 let rec resolveAndCheckDecl env (decl: Decl) =
@@ -298,7 +245,10 @@ let rec resolveAndCheckDecl env (decl: Decl) =
                 let expectedType = resolveTypeExpr env valueDef.NodeCategory.typeExpr Set.empty
                 if not (isSubtype env valueType expectedType) then failwithf "Type mismatch in value definition %s" valueDef.NodeCategory.name
                 env
-            | ValueDef.ValueSignature _ -> env // Signatures are already checked in the first pass
+            | ValueDef.ValueSignature valSig -> 
+                let expectedType = resolveTypeExpr env valSig.NodeCategory.typeExpr Set.empty
+                addVariable env valSig.NodeCategory.name expectedType
+
         ) env
     | Decl.AxiomDecl axiomDecl ->
         axiomDecl.NodeCategory.axiomDefList |> List.fold (fun env axiomDef ->
@@ -306,36 +256,6 @@ let rec resolveAndCheckDecl env (decl: Decl) =
             if not (isSubtype env axiomType TBool) then failwithf "Axiom %s must be of type Bool" axiomDef.NodeCategory.name
             env
         ) env
-
-// let rec resolveAndCheckDecl (env: TypeEnvironment) (decl: Decl) : TypeEnvironment =
-//     let: Set<string> = Set.empty
-//     match decl with
-//     | Decl.TypeDecl typeDecl ->
-//         typeDecl.NodeCategory.typeDefList |> List.fold (fun env typeDef ->
-//             match typeDef with
-//             | TypeDef.AbbrevDef abbrevDef -> 
-//                 let typ = resolveTypeExpr env abbrevDef.NodeCategory.typeExpr
-//                 addType env abbrevDef.NodeCategory.name typ
-//             | _ -> env
-//         ) env
-//     | Decl.ValueDecl valueDecl ->
-//         valueDecl.NodeCategory.valueDefList |> List.fold (fun env valueDef ->
-//             match valueDef with
-//             | ValueDef.ExplicitFunctionDef funcDef -> resolveAndCheckFunction env funcDef
-//             | ValueDef.ExplicitValueDef valueDef ->
-//                 let valueType = resolveAndCheckValueExpr env valueDef.NodeCategory.valueExpr
-//                 let expectedType = resolveTypeExpr env valueDef.NodeCategory.typeExpr
-//                 if not (isSubtype env valueType expectedType) then failwithf "Type mismatch in value definition %s" valueDef.NodeCategory.name
-//                 env
-//             | ValueDef.ValueSignature _ -> env // Signatures are already checked in the first pass
-//         ) env
-//     | Decl.AxiomDecl axiomDecl ->
-//         axiomDecl.NodeCategory.axiomDefList |> List.fold (fun env axiomDef ->
-//             let axiomType = resolveAndCheckValueExpr env axiomDef.NodeCategory.logicalValueExpr
-//             if not (isSubtype env axiomType TBool) then failwithf "Axiom %s must be of type Bool" axiomDef.NodeCategory.name
-//             env
-//         ) env
-
 
 let resolveAndCheckScheme env (scheme: Node<SchemeDecl>) =
     scheme.NodeCategory.schemeDef.NodeCategory.classExpr.NodeCategory.optDecl |> List.fold resolveAndCheckDecl env
